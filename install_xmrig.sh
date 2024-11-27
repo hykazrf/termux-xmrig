1e74ce36a451a7846f85a504822270dc73eb9ca3
hykazrf <119750578+hykazrf@users.noreply.github.com>
2024-11-27 16:30:18 +0800

    Update install_xmrig.sh

--- install_xmrig.sh
+++ install_xmrig.sh
@@ -4,8 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # 安装构建 XMRig 所需的依赖包
-sudo apt install -y automake clang git vim cmake build-essential
-sudo apt install -y libssl-dev
+sudo apt install -y automake clang git vim cmake build-essential libssl-dev
 
 # 克隆 XMRig 仓库
 git clone https://github.com/xmrig/xmrig
